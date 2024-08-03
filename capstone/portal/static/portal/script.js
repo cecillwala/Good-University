464da3946467c8295e215dde8d50325c6d7759d2
cecillwala <lwalacecil13@gmail.com>
2024-08-03 10:09:25 +0300

    sticky

--- capstone/portal/static/portal/script.js
+++ capstone/portal/static/portal/script.js
@@ -1,41 +1,49 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector("#upload").addEventListener('click', () => first_year_admin());
+document.addEventListener('DOMContentLoaded', () => {
+    ShowPage("#profile");
+    document.querySelector("#csv").addEventListener('change', () => first_year_admin());
 });
 
 
+function ShowPage(page){
+   const children = Array.from(document.querySelector("#main").children);
+    children.forEach(child => {
+        child.style.display = 'none';
+    });
+    document.querySelector(page).style.display = 'block';
+} 
+
+
 function first_year_admin(){
-    document.querySelector("#confirm").style.display = 'block';
-    document.querySelector("#not-confirmed").addEventListener('click', () => {
-        document.querySelector("#confirm").style.display = 'none';
-    });
-    document.querySelector("#confirmed").addEventListener('click', () => {
-        document.querySelector("#confirm").style.display = 'none';
-    });
+    const file_name = document.querySelector("#csv").value.split("\\");
+    document.querySelector("#upload").value = file_name[2];
+    document.querySelector("#status").outerHTML = `
+    <div id="status" class="alert alert-warning">
+        <strong>Warning!</strong> Submitting file has no reverse process.
+    </div>
+    `;
     document.querySelector("#upload-form").addEventListener('submit', (event) => {
         event.preventDefault();
-        document.querySelector("#confirm").style.display = 'none';
-        const file_name = document.querySelector("#csv").value.split("\\");
-        console.log(file_name[2]);
-            fetch('upload', {
-                method:'POST',
-                body: document.querySelector("#csv").files[0]
-            })
-            .then(response => response.json())
-            .then(status => {
-                if(status.status == 200){
-                    document.querySelector("#status").outerHTML = `
-                    <div class="alert alert-success">
-                        <strong>Success.</strong> Student's file uploaded successfully.
-                    </div>
-                    `;
-                    document.querySelector("#contents").style.display = 'none';
-                }
-                else{
-                    document.querySelector("#status").outerHTML = `
-                    <div class="alert alert-danger">
-                        <strong>Danger!</strong> Indicates a dangerous or potentially negative action.
-                    </div>`;
-                }
-            });
+        fetch('upload', {
+            method:'POST',
+            body: document.querySelector("#csv").files[0]
+        })
+        .then(response => response.json())
+        .then(status => {
+            console.log(status);
+            if(status.status == 200){
+                document.querySelector("#csv").setAttribute('disabled', true);
+                document.querySelector("#status").outerHTML = `
+                <div class="alert alert-success">
+                    <strong>Success.</strong> Student's file uploaded successfully.
+                </div>
+                `;
+            }
+            else{
+                document.querySelector("#status").outerHTML = `
+                <div class="alert alert-danger">
+                    <strong>Danger!</strong> Indicates a dangerous or potentially negative action.
+                </div>`;
+            }
+        });
     });
 }
